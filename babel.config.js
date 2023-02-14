9264ac996e03f5223dc253e116b31b21c99065f9
ckkulkarni2712 <97154955+ckkulkarni2712@users.noreply.github.com>
2023-02-14 10:23:47 +0530

    Update babel.config.js

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,11 @@
 module.exports = function(api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo','@babel/preset-typescript','@babel/preset-env'],
+    presets: ['babel-preset-expo','@babel/preset-typescript'],
+    plugins: [
+      ["@babel/plugin-proposal-class-properties", { "loose": true }],
+      ["@babel/plugin-proposal-private-methods", { "loose": true }],
+      ["@babel/plugin-proposal-private-property-in-object", { "loose": true }]
+    ],
   };
 };
